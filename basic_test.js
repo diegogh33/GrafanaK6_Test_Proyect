743290463082fe03f1ba3f765181a95224af4390
DESKTOP-BEGIBOB\Diego <dvich33@gmail.com>
2025-04-30 12:37:54 +0200

    Adding basic test for Grafana

--- basic_test.js
+++ basic_test.js
@@ -1,7 +1,25 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep } from "k6";
+import http from "k6/http";
 
-export default function () {
-  http.get('https://test.k6.io');
+// See https://grafana.com/docs/k6/latest/using-k6/k6-options/reference/
+export const options = {
+  stages: [
+    { duration: "1m", target: 20 },
+    { duration: "3m", target: 20 },
+    { duration: "1m", target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ["rate<0.02"], // http errors should be less than 2%
+    http_req_duration: ["p(95)<2000"], // 95% requests should be below 2s
+  },
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+  },
+};
+
+export default function main() {
+  let response = http.get("https://quickpizza.grafana.com");
   sleep(1);
 }
