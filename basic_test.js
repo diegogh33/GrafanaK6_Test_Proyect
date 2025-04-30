3d7c1e36054bee1627d13e47d83f15383d4d0a37
DESKTOP-BEGIBOB\Diego <dvich33@gmail.com>
2025-04-30 13:27:45 +0200

    Updating yml file

--- basic_test.js
+++ basic_test.js
@@ -5,7 +5,7 @@ import http from "k6/http";
 export const options = {
   stages: [
     { duration: "1m", target: 20 },
-    { duration: "3m", target: 20 },
+    // { duration: "3m", target: 20 },
     { duration: "1m", target: 0 },
   ],
   thresholds: {
